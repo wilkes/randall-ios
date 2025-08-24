3998e1cd58734dbc6a40e2f9263dab765e631c1d
Wilkes Joiner <wilkesjoiner@gmail.com>
2025-08-24 11:29:58 -0500

    Initial Commit

---
+++ Randall iOS/Randall_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  Randall_iOSApp.swift
+//  Randall iOS
+//
+//  Created by Wilkes Joiner on 8/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Randall_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Randall iOSTests/Randall_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Randall_iOSTests.swift
+//  Randall iOSTests
+//
+//  Created by Wilkes Joiner on 8/24/25.
+//
+
+import Testing
+
+struct Randall_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
